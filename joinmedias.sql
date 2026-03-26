c26041e04401e6432ed235d636487f0e58b7e5fa
Val <valexx55@gmail.com>
2026-03-26 12:31:48 +0100

    funciones agregación COUNT AVG GROUPBY

---
+++ joinmedias.sql
@@ -0,0 +1,95 @@
+-- Nº TOTAL DE ADMISIONES
+
+SELECT 
+    COUNT(*)
+FROM
+    admisiones;
+    
+-- PACIENTES QUE NO HAN RECIBIDO EL ALTA
+
+SELECT 
+    COUNT(*)
+FROM
+    admisiones
+WHERE fecha_alta IS NULL;
+
+SELECT 
+    COUNT(1)
+FROM
+    admisiones
+WHERE fecha_alta IS NULL;
+
+-- PACIENTES QUE SÍ HAN RECIBIDO EL ALTA
+
+SELECT 
+    COUNT(fecha_alta) -- si especificamos un campo, el count, nos cuenta sólo los que no tengan valor null
+FROM
+    admisiones;
+    
+SELECT NOW(); -- NOS DA LA FECHA HORA ACTUALES (MÁS PRECISO)   
+    
+-- Nº DE ADMISIONES EN EL ÚLTIMO MES
+SELECT 
+    COUNT(*)
+FROM
+    admisiones
+ -- filtrar las admisiones del último mes. la fecha de admisión sea mayor o igual que la fecha actual, menos un mes
+WHERE fecha_admision >=  NOW() - INTERVAL 1 MONTH;
+
+
+-- Nº DE ADMISIONES EN EL ÚLTIMO AÑO
+SELECT 
+    COUNT(*)
+FROM
+    admisiones
+ -- filtrar las admisiones del último mes. la fecha de admisión sea mayor o igual que la fecha actual, menos un mes
+WHERE fecha_admision >=  NOW() - INTERVAL 1 YEAR; 
+
+-- Nº DE ADMISIONES EN UN INTERVALO CONCRETO
+
+SELECT 
+    COUNT(*)
+FROM
+    admisiones
+WHERE fecha_admision BETWEEN '2025-01-01' AND '2026-03-26'; 
+
+-- DADO UN ID DE PACIENTE, CONSULTAMOS CUÁNTAS ADMISIONES HA TENIDO EN EL ÚLITMO MES
+
+SET @idpaciente := 1;
+SELECT 
+    COUNT(*)
+FROM
+    admisiones
+WHERE
+    paciente_id = @idpaciente
+AND
+fecha_admision >=  DATE_SUB(CURDATE(), INTERVAL 12 MONTH);
+
+SELECT CURDATE(); -- SELECCIONA LA FECHA ACTUAL (SIN LA HORA)
+
+
+-- UN LISTADO DE ALERGIAS, AGRUPADO POR SU NOMBRE Y EL NÚMERO DE PACIENTES QUE LA PADECEN
+
+SELECT 
+    alergias.nombre, COUNT(*) AS num_pacientes
+FROM
+    paciente_alergia
+        JOIN
+    alergias ON paciente_alergia.alergia_id = alergias.alergia_id
+GROUP BY alergias.nombre;
+
+
+-- OBTENEMOS LA MEDIA DE PESO DE LOS PACIENTES
+
+SELECT 
+    ROUND(AVG(pacientes.peso),1) AS peso_medio
+FROM
+    pacientes;
+
+
+-- OBTENEMOS LA MEDIA DE ALTURA DE LOS PACIENTES
+
+SELECT 
+    ROUND(AVG(pacientes.altura),2) AS altura_media
+FROM
+    pacientes;
